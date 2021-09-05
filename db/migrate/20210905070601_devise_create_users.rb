27afee4ec9fc5408680698e58b83e54b897ce598
nishimototo <dkspm2436@gmail.com>
2021-09-05 13:53:01 +0000

    新規登録とログイン画面の編集

--- db/migrate/20210905070601_devise_create_users.rb
+++ db/migrate/20210905070601_devise_create_users.rb
@@ -32,7 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name
+      t.string :profile_image_id
+      t.string :introduction
+      t.boolean :is_deleted
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 2021_09_05_070601) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "profile_image_id"
+    t.string "introduction"
+    t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
