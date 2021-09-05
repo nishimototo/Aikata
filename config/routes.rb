ad17b0350f7f04f37b9286d3d334a3de41724f90
nishimototo <dkspm2436@gmail.com>
2021-09-05 07:21:51 +0000

    devise導入、会員と管理者で分け作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
- root "posts#index"
- resources :posts
+
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+ root "articles#index"
+ resources :articles
 end
