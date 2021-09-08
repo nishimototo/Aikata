a181796f76abf9b40a01ab07e146231e87f2c9c2
nishimototo <dkspm2436@gmail.com>
2021-09-08 07:21:08 +0000

    フォロー機能非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,13 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     @follow = current_user.active_relationships.new(follower_id: @user.id)
     @follow.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
     @user = User.find(params[:user_id])
     @follow = current_user.active_relationships.find_by(follower_id: @user.id)
     @follow.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 end
