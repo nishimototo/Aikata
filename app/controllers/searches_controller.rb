80de59814b641627fb28af0987821450dcb13baa
nishimototo <dkspm2436@gmail.com>
2021-09-09 13:39:23 +0000

    searches/search実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,13 +3,13 @@ class SearchesController < ApplicationController
     @area = params[:area]
     @category = params[:category]
     if @category.present? && @area.present?
-      @articles = Article.where("area LIKE? AND category LIKE?", "#{@area}", "#{@category}")
+      @articles = Article.where("area LIKE? AND category LIKE?", "#{@area}", "#{@category}").page(params[:page]).per(5).order(created_at: :DESC)
     elsif @category.present?
-      @articles = Article.where("category LIKE?",  "#{@category}")
+      @articles = Article.where("category LIKE?",  "#{@category}").page(params[:page]).per(5).order(created_at: :DESC)
     elsif @area.present?
-      @articles = Article.where("area LIKE?",  "#{@area}")
+      @articles = Article.where("area LIKE?",  "#{@area}").page(params[:page]).per(5).order(created_at: :DESC)
     else
-      @articles = Article.all
+      @articles = Article.page(params[:page]).per(5).order(created_at: :DESC)
     end
   end
 end
