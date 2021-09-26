e8169c696c23801361fe463f3d06a6819be26914
nishimototo <dkspm2436@gmail.com>
2021-09-26 08:56:13 +0000

    コメント機能のテスト追記,users/my_chart内の記述をモデルにメソッド化

--- spec/system/answers_spec.rb
+++ spec/system/answers_spec.rb
@@ -1,4 +1,4 @@
-=begin
+
 require 'rails_helper'
 
 RSpec.describe '回答機能のテスト', type: :system do
@@ -13,14 +13,30 @@ RSpec.describe '回答機能のテスト', type: :system do
       click_button 'ログイン'
       visit new_theme_answer_path(theme.id)
     end
-    content '回答の投稿に成功する場合' do
+    context '回答の投稿に成功する場合' do
       before do
         fill_in 'answer[content]', with: Faker::Lorem.characters(number: 20)
       end
       it '自分の回答が正しく保存される' do
         expect{ click_button '回答する'}.to change(user.answers, :count).by(1)
       end
+      it '回答後、回答一覧ページにリダイレクトされる' do
+        click_button '回答する'
+        expect(current_path).to eq "/themes/#{theme.id}/answers"
+      end
+    end
+    context '回答の投稿に失敗する場合' do
+      before do
+        fill_in 'answer[content]', with: ''
+      end
+      it '自分の回答が正しく保存されない' do
+         expect{ click_button '回答する'}.not_to change(user.answers, :count)
+      end
+      it '再度、回答投稿ページへリダイレクトされる' do
+        click_button '回答する'
+        expect(current_path).to eq "/themes/#{theme.id}/answers"
+      end
     end
   end
 end
-=end
+
---
+++ spec/system/article_comments_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe '記事へのコメント機能のテスト', type: :system do
+  describe '記事に対するコメントの投稿テスト' do
+    let(:user) { create(:user) }
+    let!(:article) { create(:article, user: user) }
+    let(:article_comment) { create(:article_comment, user: user) }
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+      visit article_path(article.id)
+    end
+    context 'コメントの投稿に成功する場合' do
+      before do
+        fill_in 'article_comment[comment]', with: Faker::Lorem.characters(number: 20)
+      end
+      it '自分のコメントが正しく保存される' do
+        expect{ click_button 'コメント'}.to change(user.article_comments, :count).by(1)
+      end
+    end
+
+    context 'コメントの投稿に失敗する場合' do
+      before do
+        fill_in 'article_comment[comment]', with: ''
+      end
+      it '自分のコメントが正しく保存されない' do
+        expect{ click_button 'コメント'}.not_to change(user.article_comments, :count)
+      end
+    end
+  end
+end
